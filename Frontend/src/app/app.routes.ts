9d33e52b449103e89a670c49e6fcd6b86e094f40
mukund_nemane <120169435+mukund9766@users.noreply.github.com>
2024-03-15 19:03:32 +0530

    Update app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CategoryMasterComponent } from './category-master/category-master.component';
+import { ProductMasterComponent } from './product-master/product-master.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'categories', component: CategoryMasterComponent },
+  { path: 'products', component: ProductMasterComponent },
+  { path: '', redirectTo: '/categories', pathMatch: 'full' } 
+];
